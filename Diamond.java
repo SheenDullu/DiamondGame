15be5903b008c699fba351b8b5a033d7fdd5efb7
RohiniMaliCDK <rohinimali1994@gmail.com>
2016-07-29 18:19:43 +0530

    updated diamond class

--- Diamond.java
+++ Diamond.java
@@ -8,12 +8,16 @@ import java.util.Collections;
  */
 public class Diamond {
     public static void main(String[] args) {
-        ArrayList<Integer> DiamondCard = new ArrayList<>();
-        DiamondCard=shuffle();
+        ArrayList<Integer> humanBid = new ArrayList<>(13);
+        ArrayList<Integer> computerBid=new ArrayList<>(13);
 
-        System.out.println(DiamondCard.get(0));
-        DiamondCard.remove(0);
-        System.out.println(DiamondCard);
+        humanBid=shuffle();
+        computerBid=shuffle();
+
+        ArrayList<Integer> diamonds=new ArrayList<>();
+        diamonds=shuffle();
+        
+        CalculateScore(diamonds,humanBid,computerBid);
 
     }
 
@@ -22,10 +26,34 @@ public class Diamond {
         for(int i=2;i<15;++i){
             Diamond.add(i);
         }
-
-        System.out.println(Diamond);
         Collections.shuffle(Diamond);
 
         return Diamond;
+    }
+
+    public static void CalculateScore(ArrayList<Integer> diamondValue,ArrayList<Integer> humanBid,ArrayList<Integer> compBid){
+
+        int humanScore=0;
+        int computerScore=0;
+
+        for(int i=0;i<13;++i){
+            if(humanBid.get(i)>compBid.get(i)){
+                humanScore=humanScore + diamondValue.get(i);
+            }else if (humanBid.get(i)<compBid.get(i)){
+                computerScore=computerScore+diamondValue.get(i);
+            } else {
+                humanScore=diamondValue.get(i)/2;
+                computerScore=diamondValue.get(i)/2;
+            }
+        }
+        System.out.println("human"+ humanScore);
+        System.out.println("Computer"+computerScore);
+        if(humanScore>computerScore){
+            System.out.println("You win");
+        } else if(humanScore<computerScore){
+            System.out.println("Computer wins");
+        } else {
+            System.out.println("Tie");
+        }
     }
 }
