87627a127ece13714023b669a37f20a2ffbbad47
RohiniMaliCDK <rohinimali1994@gmail.com>
2016-07-29 18:41:14 +0530

    updated Human player

---
+++ HumanPlayer.java
@@ -0,0 +1,38 @@
+package CardsGame2;
+
+/**
+ * Created by malir on 7/29/2016.
+ */
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class HumanPlayer {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        ArrayList<Integer> humanBid=new ArrayList<>();
+
+        int tmp = 0;
+        for(int i=0; i< 13;i++) {
+            System.out.println("Enter the Amount");
+
+            tmp = scan.nextInt();
+            if(ifValid(tmp,humanBid)){
+                humanBid.add(tmp);
+            } else {
+                System.out.println("Invalid input");
+            }
+
+        }
+
+    }
+
+    public static boolean ifValid(int value,ArrayList<Integer> bidList) {
+        for(int i=0;i<bidList.size();++i){
+            if(bidList.get(i)==value){
+                return false;
+            }
+        }
+        return true;
+    }
+}
